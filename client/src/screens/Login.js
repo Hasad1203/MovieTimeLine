0fc522770c5cf3ea328a946337092a6a70e6618c
Avram Baicu <37336756+AvramBaicu@users.noreply.github.com>
2022-05-28 16:40:56 -0400

    Login Page Completed

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,7 +1,41 @@
 import "./Login.css";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="login_main">
+      <a href="" className="login_header">
+        MOVIETIMELINE.
+      </a>
+      <a href="" className="sign_up">
+        Sign up
+      </a>
+      <div className="login_background">
+        <h1>Sign In</h1>
+
+        <div>
+          <input
+            className="form_group"
+            type="text"
+            name="username"
+            id="username "
+            placeholder="Username:"
+          />
+        </div>
+        <div>
+          <input
+            className="form_group"
+            type="text"
+            name="password"
+            id="password "
+            placeholder="Password:"
+          />
+        </div>
+        <button className="login_button" value="LOGIN" src="">
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
