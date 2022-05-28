563f9be10f459c77414ebf35016dcda94b1125be
musaasad2020 <musaasad.2020@gmail.com>
2022-05-28 19:14:03 -0400

    Updates

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,12 +1,46 @@
+import Countdown from "react-countdown";
+
 const Item = (props) => {
-  const { movie_id, title, release_data, directors, plot_summary, cover_url } =
-    props;
+  const { movie_id, title, release_date, directors, cover_url } = props;
+
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    return (
+      <span>
+        {days.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+        :
+        {hours.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+        :
+        {minutes.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+        :
+        {seconds.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+      </span>
+    );
+  };
+
   return (
     <div className="item-main">
       <img className="item-image" src={cover_url} />
       <div className="item-header">{title}</div>
       <div className="item-directors">{directors}</div>
-      <div className=""></div>
+      <div className="item-countdown">
+        <Countdown
+          className="item-countdown-inner"
+          date={Date.parse(release_date)}
+          renderer={renderer}
+        />
+      </div>
     </div>
   );
 };
--- client/src/screens/Movies.js
+++ client/src/screens/Movies.js
@@ -1,86 +1,30 @@
 import "./Movies.css";
 import Item from "../components/Item";
-import Search from "../icons/Search";
+import Plus from "../icons/Plus";
 
 const Movies = () => {
   const data = [
     {
       movie_id: "1",
-      title: "The Avengers",
-      release_data: "05/30/2022",
-      directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+      title: "THOR: LOVE AND THUNDER",
+      release_date: "05/31/2022",
+      directors: ["Joss Whedon", "Jamed Webb", "Danseeee"],
       cover_url:
         "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
     },
     {
       movie_id: "1",
       title: "The Avengers",
-      release_data: "05/30/2022",
+      release_date: "05/31/2022",
       directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
       cover_url:
         "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
     },
     {
       movie_id: "1",
       title: "The Avengers",
-      release_data: "05/30/2022",
+      release_date: "05/31/2022",
       directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-      cover_url:
-        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
-    },
-    {
-      movie_id: "1",
-      title: "The Avengers",
-      release_data: "05/30/2022",
-      directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-      cover_url:
-        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
-    },
-    {
-      movie_id: "1",
-      title: "The Avengers",
-      release_data: "05/30/2022",
-      directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-      cover_url:
-        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
-    },
-    {
-      movie_id: "1",
-      title: "The Avengers",
-      release_data: "05/30/2022",
-      directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-      cover_url:
-        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
-    },
-    {
-      movie_id: "1",
-      title: "The Avengers",
-      release_data: "05/30/2022",
-      directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-      cover_url:
-        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
-    },
-    {
-      movie_id: "1",
-      title: "The Avengers",
-      release_data: "05/30/2022",
-      directors: ["Joss Whedon"],
-      plot_summary:
-        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
       cover_url:
         "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY150_CR0,0,101,150_.jpg",
     },
@@ -92,27 +36,28 @@ const Movies = () => {
         <input
           className="movies-search-input"
           type="text"
-          placeholder="Add movies"
+          placeholder="movie name"
         />
         <div className="movies-search-button">
-          <Search className="movies-search-icon" />
+          <Plus className="movies-search-icon" />
         </div>
       </div>
+      <div className="movies-title">your movie list:</div>
       <div className="movies-timeline">
         {data.map((item) => {
           return (
             <Item
               movie_id={item.movie_id}
               title={item.title}
-              release_data={item.release_data}
+              release_date={item.release_date}
               directors={item.directors}
-              plot_summary={item.plot_summary}
               cover_url={item.cover_url}
             />
           );
         })}
         <div className="movies-timeline-line" />
       </div>
+      <div className="movies-logout">Logout (musaasad2020)</div>
     </div>
   );
 };
