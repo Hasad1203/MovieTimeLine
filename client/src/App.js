a3f4edf2cc5bd54bb7a7e51fda363664987e162e
musaasad2020 <musaasad.2020@gmail.com>
2022-05-28 12:36:15 -0400

    React Router Dom
    Added the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./screens/Login";
+import Movies from "./screens/Movies";
+import Movie from "./screens/Movie";
 
 const App = () => {
-  return <div className="main"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
