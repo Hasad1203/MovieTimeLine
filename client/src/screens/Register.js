a7a54b823c71d636bab677a19ec0ac0fea4db052
Avram Baicu <37336756+AvramBaicu@users.noreply.github.com>
2022-05-28 20:44:24 -0400

    Register Page Completed

--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -7,32 +7,54 @@ const Register = () => {
         MOVIETIMELINE.
       </a>
       <a href="" className="sign_up">
-        Sign up
+        Sign In
       </a>
-      <div className="login_background">
-        <h1>Sign In</h1>
-
+      <div className="signin_background">
+        <h1>Sign Up</h1>
         <div>
           <input
-            className="form_group"
+            className="form_group_signup"
             type="text"
             name="username"
             id="username "
-            placeholder="Username:"
+            placeholder="Email Address"
           />
         </div>
         <div>
           <input
-            className="form_group"
+            className="form_group_signup"
+            type="text"
+            name="username"
+            id="username "
+            placeholder="New Username:"
+          />
+        </div>
+        <div>
+          <input
+            className="form_group_signup"
             type="text"
             name="password"
             id="password "
-            placeholder="Password:"
+            placeholder="New Password:"
+          />
+          <input
+            className="form_group_signup"
+            type="text"
+            name="password"
+            id="password "
+            placeholder="Confirm Password:"
           />
         </div>
-        <button className="login_button" value="LOGIN" src="">
-          Login
+        <button className="login_button" value="Register" src="">
+          Register
         </button>
+        <p>
+          Already have an account?
+          <span className="back_to_signin" href="">
+            {" "}
+            Sign In{" "}
+          </span>
+        </p>
       </div>
     </div>
   );
